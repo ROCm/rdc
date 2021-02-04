b88238b3bd02652cf654206a7befdd4deea039ae
Chris Freehill <Chris.Freehill@amd.com>
2021-02-04 08:17:48 -0600

    Turn on/off DAC capabilities as needed
    Write access is required for some RSMI services. This change
    temporarily permits write access so configuration can be done,
    and then turns it off.
    
    To help with this, the ScopedCapability struct is introduced to
    provide scope limited access, helping to ensure a process is not
    left with extra capability, should an exception occur.
    
    Change-Id: I4978a1a688db935b8bfc27b3b537a0dd07959d3f

--- common/rdc_capabilities.cc
+++ common/rdc_capabilities.cc
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020 - present Advanced Micro Devices, Inc. All rights reserved.
+Copyright (c) 2021 - present Advanced Micro Devices, Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,8 @@ THE SOFTWARE.
 #include <sys/capability.h>
 #include <errno.h>
 #include <assert.h>
+
+#include "common/rdc_capabilities.h"
 
 namespace amd {
 namespace rdc {
