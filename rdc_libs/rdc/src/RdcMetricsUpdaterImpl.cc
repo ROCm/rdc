eea5b52a254ab5e9d6e107ddf819e5ceee19ed89
Bill(Shuzhou) Liu <shuzhou.liu@amd.com>
2021-10-26 15:36:52 -0400

    Fix the compile error for gcc-11
    Fix the error: 'sleep_for' is not a member of 'std::this_thread'
    
    Change-Id: If25ef03023df17081878f9b44c3a68195f07c653
    
    
    [ROCm/rdc commit: adfa89631d487f34786170d931d519644f89b16a]

--- rdc_libs/rdc/src/RdcMetricsUpdaterImpl.cc
+++ rdc_libs/rdc/src/RdcMetricsUpdaterImpl.cc
@@ -22,6 +22,7 @@ THE SOFTWARE.
 #include "rdc_lib/impl/RdcMetricsUpdaterImpl.h"
 #include <sys/time.h>
 #include <ctime>
+#include <thread>
 #include <chrono>  // NOLINT(build/c++11)
 #include "rdc_lib/rdc_common.h"
 
