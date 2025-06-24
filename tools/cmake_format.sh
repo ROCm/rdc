98d90e57bd0599c97616d77ffece7b7a50d73642
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2025-06-24 17:32:55 -0500

    CMAKE - Add gersemi to replace cmake-format
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
    
    
    [ROCm/rdc commit: 680b7a8dd8699ab5264f10f5a3bc0b30410881be]

--- tools/cmake_format.sh
+++ tools/cmake_format.sh
@@ -4,7 +4,14 @@ set -e
 set -u
 set -o pipefail
 
-FILES=$(find . -type f \( -name "CMakeLists.txt" -o -name "*.cmake" -o -name "*.cmake.in" \) \
+if ! command -v gersemi >/dev/null 2>&1; then
+    echo "$(tput setaf 1)gersemi is not installed. Please install it to format CMake files.$(tput sgr0)"
+    echo -e "$(tput setaf 6)try this:\n    python3 -m pip install gersemi$(tput sgr0)"
+    exit 1
+fi
+
+FILES=$(find . -type f \( -name "CMakeLists.txt" -o -name "*.cmake" \) \
+    -not -name "*.in" \
     -not -path "*/\.*" \
     -not -path "*/build/*")
 
@@ -13,12 +20,12 @@ failed_files=()
 # Check if files are formatted correctly
 for file in $FILES; do
     echo "Checking $file..."
-    if ! cmake-format --check "$file"; then
+    if ! gersemi --check "$file"; then
         failed_files+=("$file")
         echo "::error file=$file::File needs formatting"
     fi
 done
 
 if [ ${#failed_files[@]} -ne 0 ]; then
-    cmake-format -i "${failed_files[@]}"
+    gersemi -i "${failed_files[@]}"
 fi
