bcac1441fa0a88d02c8a8d871a8b02903a6a8340
Dmitrii <19967783+dmitrii-galantsev@users.noreply.github.com>
2025-09-24 16:08:02 +0000

    chore: [rdc] Add copyright notice [rocm-systems] ROCm/rocm-systems#1098 (commit 0575606)

--- python_binding/rdc_collectd.py
+++ python_binding/rdc_collectd.py
@@ -1,3 +1,6 @@
+# Copyright © Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier:  MIT
+
 from RdcReader import RdcReader
 from rdc_bootstrap import *
 import collectd
--- server/run_build.sh
+++ server/run_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright © Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier:  MIT
+
 mkdir -p build
 cd build
 cmake -DROCM_DIR=/opt/rocm ..
