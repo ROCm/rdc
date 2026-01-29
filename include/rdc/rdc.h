a81e330ca85e5ed2cb951b0766a875546c6388e4
Adam Pryor <61172547+adam360x@users.noreply.github.com>
2026-01-29 16:27:24 -0600

    [SWDEV-566924] Update KFD_ID metric to use amd-smi instead of rocprof (#2355) (#2387)

--- include/rdc/rdc.h
+++ include/rdc/rdc.h
@@ -171,6 +171,7 @@ typedef enum {
   RDC_FI_NUM_OF_COMPUTE_UNITS,     //!< Number of compute units
   RDC_FI_UUID,                     //!< Device UUID
   RDC_FI_GPU_PARTITION_COUNT,
+  RDC_FI_KFD_ID,
 
   /**
    * @brief Frequency related fields
@@ -344,7 +345,6 @@ typedef enum {
   RDC_FI_PROF_CPF_CPF_TCIU_IDLE,
   RDC_FI_PROF_CPF_CPF_TCIU_STALL,
   RDC_FI_PROF_SIMD_UTILIZATION,
-  RDC_FI_PROF_KFD_ID,
 
   /**
    * @brief Raw XGMI counter events
