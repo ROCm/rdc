bde87f21f8fe6c63526934066f8aededb814361b
JeniferC99 <150404595+JeniferC99@users.noreply.github.com>
2026-01-30 16:17:00 -0800

    Revert "[SWDEV-566924] Update KFD_ID metric to use amd-smi instead of rocprof…" (#3003)
    This reverts commit b11e0c16e1762d28df4677aa4ec0f6aee6f44222.

--- rdc_libs/rdc_modules/rdc_rocp/RdcRocpBase.cc
+++ rdc_libs/rdc_modules/rdc_rocp/RdcRocpBase.cc
@@ -108,6 +108,7 @@ static const std::map<rdc_field_t, const char*> temp_field_map_k = {
     {RDC_FI_PROF_CPF_CPF_TCIU_IDLE, "CPF_CPF_TCIU_IDLE"},
     {RDC_FI_PROF_CPF_CPF_TCIU_STALL, "CPF_CPF_TCIU_STALL"},
     {RDC_FI_PROF_SIMD_UTILIZATION, "SIMD_UTILIZATION"},
+    {RDC_FI_PROF_KFD_ID, "SQ_WAVES"},  // dummy value,
 };
 
 double RdcRocpBase::run_profiler(uint32_t agent_index, rdc_field_t field) {
@@ -558,6 +559,12 @@ rdc_status_t RdcRocpBase::apply_field_transformation(
       }
       output->dbl = divided_dbl / (256.0F / static_cast<double>(agents[agent_index].simd_per_cu));
       break;
+
+    case RDC_FI_PROF_KFD_ID:
+      *type = INTEGER;
+      output->l_int = agents[agent_index].gpu_id;
+      break;
+
     default:
       if (is_eval_field) {
         output->dbl = divided_dbl;
